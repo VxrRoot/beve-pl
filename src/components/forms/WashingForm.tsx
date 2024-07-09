60cae7403dd0e615accacc9c868e8996f7029c5a
Dawid Slowik <dawidslowik379@gmail.com>
2024-07-09 15:37:08 +0200

    Washing form update

--- src/components/forms/WashingForm.tsx
+++ src/components/forms/WashingForm.tsx
@@ -12,6 +12,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "../ui/button";
 import { DM_Mono } from "next/font/google";
 import {
@@ -25,10 +26,11 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 import { Calendar } from "../ui/calendar";
+import { Textarea } from "../ui/textarea";
 
 const mono = DM_Mono({ weight: ["500"], subsets: [] });
 
-const FormSchema = z.object({
+export const FormWashingSchema = z.object({
   serviceType: z.enum(["mycie cykliczne", "mycie jednorazowe"], {
     required_error: "To pole jest wymagane",
   }),
@@ -42,23 +44,74 @@ const FormSchema = z.object({
   cupProducer: z.string().optional(),
   sendDate: z.date({ required_error: "Data jest wymagana" }),
   pickupDate: z.date({ required_error: "Data jest wymagana" }),
+  nip: z
+    .string({ required_error: "Numer NIP jest wymagany" })
+    .min(9, { message: "Wpisz poprawny numer NIP" }),
+  companyName: z
+    .string({ required_error: "Nazwa firmy jest wymagana" })
+    .min(2, "Wpisz poprawną nazwę firmy"),
+  country: z
+    .string({ required_error: "Miasto jest wymagane" })
+    .min(2, { message: "Wpisz poprawną nazwę kraju" }),
+  postCode: z
+    .string({ required_error: "Kod pocztowy jest wymagany" })
+    .min(2, { message: "Wpisz poprawny kod pocztowy" }),
+  city: z
+    .string({ required_error: "Miasto jest wymagane" })
+    .min(2, { message: "Wpisz poprawną nazwę" }),
+  street: z
+    .string({ required_error: "Ulica jest wymagana" })
+    .min(2, { message: "Wpisz poprawną nazwę" }),
+  buildingNumber: z
+    .string({ required_error: "Numer budynku jest wymagany" })
+    .min(2, { message: "Wpisz poprawną nazwę" }),
+  flatNumber: z.string().optional(),
+  invoiceDataAreShipment: z.boolean().default(false).optional(),
+  nameAndSurname: z.string().min(2, "Wpisz imię i nazwisko"),
+  email: z
+    .string({ required_error: "Wpisz adres email" })
+    .email({ message: "Wpisz poprawny adres email" }),
+  phone: z
+    .string({ required_error: "Wpisz numer telefonu" })
+    .min(9, { message: "Wpisz poprawny numer telefonu" }),
+  message: z.string().optional(),
+  termsConsent: z
+    .boolean({ required_error: "Zgoda jest wymagana" })
+    .default(false)
+    .refine((val) => val === true, {
+      message: "Musisz zaakceptować politykę prywatności",
+    }),
 });
 
+export type FormSchemaType = z.infer<typeof FormWashingSchema>;
+
 const WashingForm = () => {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(FormWashingSchema),
     defaultValues: {
       serviceType: "mycie cykliczne",
       boxes: "posiadam skrzynki",
       cupAmount: "",
       boxesAmount: "",
       cupProducer: "",
+      flatNumber: "",
+      nip: "",
+      companyName: "",
+      buildingNumber: "",
+      city: "",
+      country: "",
+      street: "",
+      postCode: "",
+      nameAndSurname: "",
+      email: "",
+      phone: "",
+      message: "",
       // pickupDate: new Date(),
       // sendDate: new Date(),
     },
   });
 
-  function onSubmit(values: z.infer<typeof FormSchema>) {
+  function onSubmit(values: z.infer<typeof FormWashingSchema>) {
     console.log(values);
   }
 
@@ -274,8 +327,226 @@ const WashingForm = () => {
             </div>
           </div>
           <div className="flex-1 lg:pl-10 ">
+            <h2 className="text-2xl tracking-[-0.5px] mb-6 mt-10 lg:mt-0">
+              2. Dane firmy
+            </h2>
+            <div className="mt-3 flex flex-col gap-4 md:flex-row">
+              <FormField
+                control={form.control}
+                name="nip"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">NIP</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="companyName"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">Nazwa firmy</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mt-3 flex flex-col gap-4 md:flex-row">
+              <FormField
+                control={form.control}
+                name="country"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">Kraj</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="postCode"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">
+                      Kod pocztowy
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mt-3 flex flex-col gap-4 md:flex-row">
+              <FormField
+                control={form.control}
+                name="city"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">Miejscowość</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="street"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">Ulica</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mt-3 flex flex-col gap-4 md:flex-row">
+              <FormField
+                control={form.control}
+                name="buildingNumber"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">Nr budynku</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="flatNumber"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">
+                      Nr mieszkania
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="invoiceDataAreShipment"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-center my-6 space-x-3 space-y-0">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 text-[14px]">
+                    <FormLabel>Dane do faktury są danymi do wysyłki</FormLabel>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <h2 className="text-2xl tracking-[-0.5px] mb-6 mt-10 lg:mt-0">
+              3. Dane kontaktowe
+            </h2>
+            <div className="mt-3 flex flex-col gap-4 md:flex-row">
+              <FormField
+                control={form.control}
+                name="nameAndSurname"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">
+                      Imię i nazwisko
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mt-3 flex flex-col gap-4 md:flex-row">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">
+                      Adres e-mail
+                    </FormLabel>
+                    <FormControl>
+                      <Input type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-[.75rem]">Nr telefonu</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mt-3 flex flex-col  w-full gap-4 md:flex-row">
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Wiadomość</FormLabel>
+                    <FormControl>
+                      <Textarea className="resize-none w-full" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="termsConsent"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-center my-6 space-x-3 space-y-0">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 text-[14px]">
+                    <FormLabel>Zgadzam się na politykę prywatności</FormLabel>
+                  </div>
+                </FormItem>
+              )}
+            />
             <Button
-              className={`uppercase w-full text-base py-3 hover:bg-primaryGreen tracking-[0.84px] bg-secondaryGreen ${mono.className}`}
+              className={`uppercase w-full my-3 text-base py-3 hover:bg-primaryGreen tracking-[0.84px] bg-secondaryGreen ${mono.className}`}
               type="submit"
             >
               WYŚLIJ ABY OTRZYMAĆ OFERTĘ <ArrowUpRight className="ml-2 " />
