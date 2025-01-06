6fb8fcd04d29d5c93a9b92ac672223202c55e20b
Dawid Slowik <dawidslowik379@gmail.com>
2025-01-07 00:27:27 +0100

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import type { Metadata } from "next";
 import { DM_Mono, DM_Sans } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const sans = DM_Sans({ subsets: ["latin"] });
 
@@ -18,11 +19,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/Favicon.ico" sizes="any" />
+      <head>
+        <link rel="icon" href="/Favicon.ico" sizes="any" />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NZD8646B');
+            `,
+          }}
+        />
+      </head>
       <body
         suppressHydrationWarning={true}
         className={`${sans.className} bg-white text-black`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NZD8646B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         {children}
         <Footer />
